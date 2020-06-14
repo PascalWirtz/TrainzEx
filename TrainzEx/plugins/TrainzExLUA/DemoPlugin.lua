d5f4a610171372969e0fc9e45b48d325289a009d
Pascal Wirtz <info@pascal-wirtz.de>
2020-06-14 11:11:23 +0200

    Introducing TrainzExLUA Plugin v0.1

---
+++ TrainzEx/plugins/TrainzExLUA/DemoPlugin.lua
@@ -0,0 +1,40 @@
+--
+--	AVAILABLE CALLBACKS
+--
+
+-- 	PostMessage(major, minor)
+-- 	With PostMessage you can post messages back into Trainz
+
+--	AddHandler
+--	Please only call this function in OnInit.
+
+--	RemoveHandler
+--	Please only call this function in OnExit.
+
+
+
+--
+--	OnInit
+--	Called once the on startup
+--
+function OnInit()
+	AddHandler("Interface", "")				-- Register myself for all Interface messages
+end
+
+--
+--	OnExit
+--	Called once when shutting down
+--
+function OnExit()
+	RemoveHandler("Interface", "")			-- Unregister myself from all Interface messages
+end
+
+--
+--	OnMessage
+--	Called when within a message is posted that matches one of the handlers registered by AddHandler
+--		major: Major string of the message
+--		minor: Minor string of the message
+--
+function OnMessage(major, minor)
+											-- No you will receive all Interface messages trough this function
+end
